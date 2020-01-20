25c9ad2938ef70b9034fe4cf6f4b4f19f824829c
jaymaina <jaymaina5@gmail.com>
2020-01-21 01:30:13 +0300

    added views, controllers and models

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -1,16 +1,45 @@
 class UsersController < ApplicationController
-    before_action only: %i[create new]
+    before_action :set_user, only: [:show, :edit, :update]
 
     def index
-
+        @users = User.all
     end
 
     def new
-        @user
+        @user = User.new
     end
+
+    def show
+
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
 
     def create
-
+        #@user = User.new( username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
+        if @user.save
+            #flash[:notice] = "User was successfully created"
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password )
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 
 end
