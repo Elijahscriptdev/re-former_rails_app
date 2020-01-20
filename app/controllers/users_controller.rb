ad8eb0b8587f52325a21ea8ff978464541e94c44
jaymaina <jaymaina5@gmail.com>
2020-01-21 01:55:29 +0300

    corrected linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,40 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: %i[show edit update]
 
-    def index
-        @users = User.all
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def create
+    # @user = User.new( username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      # flash[:notice] = "User was successfully created"
+      redirect_to user_path(@user)
+    else
+      render :new
     end
+  end
 
-    def new
-        @user = User.new
-    end
+  private
 
-    def show
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
-    end
-
-    def edit
-    end
-
-    def update
-    end
-
-
-    def create
-        #@user = User.new( username: params[:username], email: params[:email], password: params[:password])
-        @user = User.new(user_params)
-        if @user.save
-            #flash[:notice] = "User was successfully created"
-            redirect_to user_path(@user)
-        else
-            render :new
-        end
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password )
-    end
-
-    def set_user
-        @user = User.find(params[:id])
-    end
-
-
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
