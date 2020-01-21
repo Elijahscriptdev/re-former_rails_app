3bdd90368b8a752b39e1536f20708ed39c9669c6
Elijah Obominuru <elijjaaahhh@gmail.com>
2020-01-21 11:33:49 +0100

    fix lint errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-            redirect_to user_path(@user)
+      redirect_to user_path(@user)
     else
       render 'new'
     end
